2e4bec024832aeab4ff5445905d83ae06644c882
TristanMueller <44208701+TristanMueller@users.noreply.github.com>
2018-10-24 23:59:43 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,4 +8,22 @@ username = input("Username: ")
 password = input("Password: ")
 
 App.login(username,password)
-App.createAccount("TA","TA",4)
+Command = input("Command: ")
+while Command!="Exit":
+  if Command == "Create Account":
+    username = input("Username: ")
+    password = input("Password: ")
+    clearance = input("Clearance: ")
+    if clearance == "Supervisor":
+      App.createAccount(username,password,1)
+    elif clearance == "Administrator":
+      App.createAccount(username,password,2)
+    elif clearance == "Professor":
+      App.createAccount(username,password,3)
+    elif clearance == "TA":
+      App.createAccount(username,password,4)
+    else:
+      print("Invalid clearance")
+  else:
+    print("Invalid Command")
+  Command = input("Command: ")
