3288ecc98f47a08945479faa3bab4caa9fcfdb90
TristanMueller <44208701+TristanMueller@users.noreply.github.com>
2018-10-24 23:16:18 -0500

    Create TASchedulingApp.py

---
+++ TASchedulingApp.py
@@ -0,0 +1,26 @@
+from User import User
+class TASchedulingApp:
+  LoggedInUser = "Null"
+  def __init__(self):
+    LoggedInUser = "Null"
+  
+  def login(self,sUsername,sPassword):
+    file_object = open("Accounts.txt","r")
+    for line in file_object:
+      lsUser = line.split(",")
+      if(lsUser[0]==sUsername and lsUser[1]==sPassword):
+        self.LoggedInUser = User(sUsername,sPassword,int(lsUser[2]))
+    if self.LoggedInUser == "Null":
+      print("could not login")
+    else:
+      print("Logged In!")
+
+  def createAccount(self,sUsername,sPassword,iClearance):
+    if self.LoggedInUser != "Null" and self.LoggedInUser.clearance < 3 :
+      oUser = User(sUsername,sPassword,iClearance)
+      file_object = open("Accounts.txt","a")
+      file_object.write(sUsername + "," + sPassword + "," + str(iClearance) + "\n")
+      file_object.close()
+      print("Created Account")  
+    else:
+      print("Invalid command")
