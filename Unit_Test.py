42674e36903b111c4a8f3750f7b09eb03c396cd1
dummer-isaiah <44410836+dummer-isaiah@users.noreply.github.com>
2018-11-06 17:01:13 -0600

    Update Unit_Test.py

--- Unit_Test.py
+++ Unit_Test.py
@@ -1,21 +1,32 @@
 import unittest
 class Testcode(unittest.TestCase):
-  login = Admin
-  login2 = NULL
+
+  from TASchedulingApp import TASchedulingApp
+
+  App = TASchedulingApp()
+
+  login = "Admin"
+  login2 = "TA"
   sTA = "John"
   iCourse = "361"
   isec = "1"
   iCourse2 = "431"
-  isec2 = "5"
-  def add_ta_succesful()
-     self.assertEqual(login.assignTA(self,sTA,iCourse,isec),"TA added to lab")
-  def add_ta_no_lab(self,sTA,iCourse,isec)
-    self.assertEqual(login.assignTA(self,sTA,iCourse,isec2),"Lab does not exist")
-  def add_ta_no_course(self,sTA,iCourse,isec)
-     self.assertEqual(login.assignTA(self,sTA,iCourse2,isec),"course does not exist")
-  def add_ta_no_login(self,sTA,iCourse,isec)
-     self.assertEqual(login2.assignTA(self,sTA,iCourse,isec),"Invalid command")
-  suite = unittest.TestSuite()
+  sTA2 = "Jim"
+  App.login(NULL,NULL)
+  def create_lab_invalid_login(sTA,iCourse,isec)
+     self.assertEqual(App.createlab(sTA,iCourse,isec),"Invalid command")
+  App.login(login2,"TA")
+   def create_lab_invalid_clearance(sTA,iCourse,isec)
+     self.assertEqual(App.createlab(sTA,iCourse,isec),"Invalid command")
+  App.login(login,"Admin")
+  def create_lab_succesful(sTA,iCourse,isec)
+     self.assertEqual(App.createlab(sTA,iCourse,isec),"Lab Created")
+  def create_lab_no_class(sTA,iCourse2,isec)
+     self.assertEqual(App.createlab(sTA,iCourse2,isec),"Course does not exist")
+  def create_lab_succesful(sTA2,iCourse,isec)
+     self.assertEqual(App.createlab(sTA2,iCourse,isec),"TA does not exist")
+
+suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(Testcode))
 runner = unittest.TextTestRunner()
 res=runner.run(suite)
